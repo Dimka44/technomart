c900982be3a5be70e271793322e77e8723f1b1d1
dimka44 <demos_32@mail.ru>
2017-11-04 00:54:01 +0300

    пытаюсь сделать js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
+link.addEventListener("click", function(e) {
+  OpenModal('.link', '.modal')
+});
+
+(function OpenModal(link, modal) {
 var link = document.querySelector('.link');
 var modal = document.querySelector(".modal");
 var name = modal.querySelector("[name=nam]")
 var closeModal = modal.querySelector('.close');
 
 link.addEventListener("click", function(e) {
-	e.preventDefault();
-	modal.classList.toggle("open");
-	nam.focus();
+  e.preventDefault();
+  modal.classList.toggle("open");
+  nam.focus();
 });
 
 closeModal.addEventListener('click', function(){
-	modal.classList.remove('open');
+  modal.classList.remove('open');
 });
+})
 
 
 
 var link2 = document.querySelector('.link2');
-var modal = document.querySelector(".modal-catalog");
-var closeModal = modal.querySelector('.close2');
+var modal2 = document.querySelector(".modal-catalog");
+var closeModal2 = modal2.querySelector('.close2');
 
-link.addEventListener("click", function(e) {
+link2.addEventListener("click", function(e) {
 	e.preventDefault();
-	modal.classList.toggle("open2");
+	modal2.classList.toggle("open2");
 });
 
-closeModal.addEventListener('click', function(){
-	modal.classList.remove('open2');
+closeModal2.addEventListener('click', function(){
+	modal2.classList.remove('open2');
 });
 
 
